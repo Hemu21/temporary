f547fcd7eca0f25c6d2a5060cc73f9422c9dcabf
Hemu21 <rajva5727@gmail.com>
2024-07-17 11:38:15 +0530

    update

--- frontend/src/components/UserData.jsx
+++ frontend/src/components/UserData.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { useEffect, useMemo, useState } from "react";
 
 const UserData = ({ user, onUpdate }) => {
--- frontend/src/components/UserForm.jsx
+++ frontend/src/components/UserForm.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 
 const UserForm = ({ onSubmit, gssocPrsCount }) => {
   const [username, setUsername] = useState("");
@@ -11,7 +12,7 @@ const UserForm = ({ onSubmit, gssocPrsCount }) => {
   return (
     <div className="w-full pt-3">
       <h1 className="text-2xl text-center mt-4 font-bold">GitHub User Data</h1>
-      <h5 className="text-xl text-center mt-4 font-semibold"><span className="text-red-600 text-xl underline">NOTE:</span> Total GSSOC merged PRs in 2024 by contributors are <span className="underline font-bold text-2xl">{gssocPrsCount}</span></h5>
+      {gssocPrsCount && <h5 className="text-xl text-center mt-4 font-semibold"><span className="text-red-600 text-xl underline">NOTE:</span> Total GSSOC merged PRs in 2024 by contributors are <span className="underline font-bold text-2xl">{gssocPrsCount}</span></h5>}
       <form onSubmit={handleSubmit} className="mt-6 flex justify-center m-2">
         <div className="m-3 w-[90%]">
           <input
